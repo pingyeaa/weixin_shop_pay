dd79eb693e903eeb03fe18ebd66713b9ac04c1b5
张以诺 <pingyeaa@163.com>
2020-10-24 12:36:04 +0800

    完成图片上传接口调

--- common/common_test.go
+++ common/common_test.go
@@ -4,17 +4,16 @@ import (
 	"log"
 	"testing"
 
-	"github.com/pingyeaa/weixin_shop_pay/params"
-
 	"github.com/pingyeaa/weixin_shop_pay/config"
+	"github.com/pingyeaa/weixin_shop_pay/params"
 )
 
 func TestCommon_ImageUpload(t *testing.T) {
 	common := Common{Config: &config.Config{
 		SpAppID:  "",
-		SpMchID:  "1603361319",
-		KeyPath:  "/Users/zhangyinuo/Golang/hdzs-api-go/cert/service_provider/apiclient_key.pem",
-		SerialNo: "6F7BFF8A4B560251EA5FA28FE961A8ABA4DDA063",
+		SpMchID:  "",
+		KeyPath:  "",
+		SerialNo: "",
 	}}
 	resp, err := common.ImageUpload(&params.CommonImageUpload{
 		FilePath: "/Users/zhangyinuo/Golang/weixin-shop-pay/test.jpg",
