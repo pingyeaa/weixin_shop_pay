fb4ad35d623ab1bc98060dcbcb27cc349472f19d
zhiyuan <617179702@qq.com>
2020-11-04 15:27:20 +0800

    删除银行号

--- params/params.go
+++ params/params.go
@@ -461,7 +461,7 @@ type EcommerceModifySettlementBody struct {
 	AccountBank     string `json:"account_bank"`      // 开户银行
 	BankAddressCode string `json:"bank_address_code"` // 开户银行省市编码
 	BankName        string `json:"bank_name"`         // 开户银行全称（含支行）
-	BankBranchID    string `json:"bank_branch_id"`    // 开户银行联行号
+	//BankBranchID    string `json:"bank_branch_id"`    // 开户银行联行号
 	AccountNumber   string `json:"account_number"`    // 银行账号
 }
 
