00fbaa51eb4ca05c8af59696b643f5079d06d2a1
zhiyuan <617179702@qq.com>
2020-11-09 16:38:28 +0800

    更新银行卡返回格式

--- ecommerce/ecommerce.go
+++ ecommerce/ecommerce.go
@@ -157,7 +157,7 @@ func (c *Ecommerce) ModifySettlement(p *params.EcommerceModifySettlement) error 
 
 	// 验证接口是否错误
 	if resp.StatusCode != 204 {
-		return errors.New("修改结算账号接口请求异常：" + string(respData))
+		return errors.New(string(respData))
 	}
 	return nil
 }
@@ -180,7 +180,7 @@ func (c *Ecommerce) QuerySettlement(p *params.EcommerceQuerySettlement) (*params
 
 	// 验证接口是否错误
 	if resp.StatusCode != 200 {
-		return nil, errors.New("查询结算信息接口请求异常：" + string(respData))
+		return nil, errors.New(string(respData))
 	}
 
 	log.Println(string(respData))
