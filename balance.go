fb85753aa0de49d246d3109004b6a05cdb3ab923
zhiyuan <617179702@qq.com>
2021-02-04 11:12:21 +0800

    日期账户

--- balance.go
+++ balance.go
@@ -43,12 +43,13 @@ func (t *Balance) SubMch(p *BalanceSubMch) (*BalanceSubMchResp, error) {
 	return &output, nil
 }
 
-// SubMchData二级商户按日期余额查询 https://api.mch.weixin.qq.com/v3/ecommerce/fund/enddaybalance/1222212222?date=2019-08-17
-func (c *Balance) SubMchData(p *params.BalanceSubMch) (*params.BalanceSubMchResp, error) {
+// urlPath := "/v3/ecommerce/fund/enddaybalance/" + p.SubMchid + "?date=" + p.Date
+// SubMchDate二级商户按日期余额查询 https://api.mch.weixin.qq.com/v3/ecommerce/fund/enddaybalance/1222212222?date=2019-08-17
+func (t *Balance) SubMchDate(p *BalanceSubMch) (*BalanceSubMchResp, error) {
 
 	// 发起请求
 	urlPath := "/v3/ecommerce/fund/enddaybalance/" + p.SubMchid + "?date=" + p.Date
-	resp, err := tools.GetRequest(c.Config, urlPath)
+	resp, err := tool.GetRequest(t.client.config, urlPath)
 	if err != nil {
 		return nil, err
 	}
@@ -58,15 +59,16 @@ func (c *Balance) SubMchData(p *params.BalanceSubMch) (*params.BalanceSubMchResp
 	if err != nil {
 		return nil, err
 	}
-
-	// 验证接口是否错误
 	if resp.StatusCode != 200 {
-		return nil, errors.New("余额日期查询接口请求异常：" + string(respData))
+		err := t.client.setErrorResponse(respData)
+		if err != nil {
+			return nil, err
+		}
 	}
 
 	// 赋值返回
 	log.Println(string(respData))
-	var output params.BalanceSubMchResp
+	var output BalanceSubMchResp
 	err = json.Unmarshal(respData, &output)
 	if err != nil {
 		return nil, err
