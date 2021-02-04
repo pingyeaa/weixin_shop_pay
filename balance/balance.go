0591187acba035f3c223411a7e3b55b782fc5694
zhiyuan <617179702@qq.com>
2021-02-04 12:04:23 +0800

    v1.3.0-v

--- balance/balance.go
+++ balance/balance.go
@@ -47,3 +47,34 @@ func (c *Balance) SubMch(p *params.BalanceSubMch) (*params.BalanceSubMchResp, er
 	}
 	return &output, nil
 }
+
+// SubMchDate 二级商户余额查询
+func (c *Balance) SubMchDate(p *params.BalanceSubMch) (*params.BalanceSubMchResp, error) {
+
+	// 发起请求
+	urlPath := "/v3/ecommerce/fund/balance/" + p.SubMchid + "?date=" + p.Date
+	resp, err := tools.GetRequest(c.Config, urlPath)
+	if err != nil {
+		return nil, err
+	}
+
+	// 解析返回参数
+	respData, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	// 验证接口是否错误
+	if resp.StatusCode != 200 {
+		return nil, errors.New("余额查询接口请求异常：" + string(respData))
+	}
+
+	// 赋值返回
+	log.Println(string(respData))
+	var output params.BalanceSubMchResp
+	err = json.Unmarshal(respData, &output)
+	if err != nil {
+		return nil, err
+	}
+	return &output, nil
+}
